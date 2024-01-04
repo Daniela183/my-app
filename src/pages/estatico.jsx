646cc78e3af879273d81395a6084744b6217e13e
Daniela Soares <danielasmelo232@gmail.com>
2024-01-04 00:08:25 -0300

    projeto estatico

---
+++ src/pages/estatico.jsx
@@ -0,0 +1,36 @@
+export const getStaticProps = async () => {
+    const resp = await fetch('http://localhost:3000/api/random')
+    const dados = await resp.json()
+
+    return {
+        revalidate: 10,
+        props: {
+            valor: dados.valor
+        }
+    }
+}
+
+const Estatico = props => {
+
+   // const [valor, setValor] = useState(0)
+//
+   // async function obterValor() {
+   //     const resp = await fetch('/api/random')
+   //     const dados = await resp.json()
+   //     setValor(dados.valor)
+   // }
+//
+   // useEffect(() => {
+   //     obterValor();
+   // }, [])
+
+
+    return (
+        <div>
+            <h1>Conteúdo Estático</h1>
+            <h2>Último valor = {props.valor}</h2>
+        </div>
+    )
+}
+
+export default Estatico
